d0399f854d4c0d94b68b15ef9410b3d0eb367b29
FrontEnd-husanboy14 <133035266+FrontEnd-husanboy14@users.noreply.github.com>
2023-11-28 15:12:45 +0500

    Add files via upload

---
+++ Husanboy.js
@@ -0,0 +1,295 @@
+// Task - 1 
+
+// let user = {
+//     name: 'Jon',
+//     surname: 'Smit'
+// }
+// user["name"] = 'Petga'
+// delete user.name
+// console.log(user)
+
+// Task - 2 
+
+// let chessBord = prompt('Katakchani kiriting')
+// switch(chessBord){
+//     // A - Section 
+//     case "A1":
+//         console.log('Black');
+//         break;
+//     case "A2":
+//         console.log('White');
+//         break;       
+//     case "A3":
+//         console.log('Black');
+//         break;
+//     case "A4":
+//         console.log('White');
+//         break;
+//     case "A5":
+//         console.log('Black');
+//         break;
+//     case "A6":
+//         console.log('White'); 
+//         break;      
+//     case "A7":
+//         console.log('Black');
+//         break;
+//     case "A8":
+//         console.log('White');
+//         break; 
+    
+//     // B - Section 
+
+//     case "B1":
+//         console.log('White');
+//         break;       
+//     case "B2":
+//         console.log('Black');
+//         break;
+//     case "B3":
+//         console.log('White');
+//         break;
+//     case "B4":
+//         console.log('Black');
+//         break;
+//     case "B5":
+//         console.log('White'); 
+//         break;      
+//     case "B6":
+//         console.log('Black');
+//         break;
+//     case "B7":
+//         console.log('White');
+//         break;   
+//     case "B8":
+//         console.log('Black');
+//         break;
+
+//     //  C - Section 
+
+//     case "C1":
+//         console.log('Black');
+//         break;
+//     case "C2":
+//         console.log('White');
+//         break;       
+//     case "C3":
+//         console.log('Black');
+//         break;
+//     case "C4":
+//         console.log('White');
+//         break;
+//     case "C5":
+//         console.log('Black');
+//         break;
+//     case "C6":
+//         console.log('White'); 
+//         break;      
+//     case "C7":
+//         console.log('Black');
+//         break;
+//     case "C8":
+//         console.log('White');
+//         break; 
+    
+//     // D - Section 
+
+//     case "D1":
+//         console.log('White');
+//         break;       
+//     case "D2":
+//         console.log('Black');
+//         break;
+//     case "D3":
+//         console.log('White');
+//         break;
+//     case "D4":
+//         console.log('Black');
+//         break;
+//     case "D5":
+//         console.log('White'); 
+//         break;      
+//     case "D6":
+//         console.log('Black');
+//         break;
+//     case "D7":
+//         console.log('White');
+//         break;   
+//     case "D8":
+//         console.log('Black');
+//         break; 
+          
+//     // E - Section 
+    
+//     case "E1":
+//         console.log('Black');
+//         break;
+//     case "E2":
+//         console.log('White');
+//         break;       
+//     case "E3":
+//         console.log('Black');
+//         break;
+//     case "E4":
+//         console.log('White');
+//         break;
+//     case "E5":
+//         console.log('Black');
+//         break;
+//     case "E6":
+//         console.log('White'); 
+//         break;      
+//     case "E7":
+//         console.log('Black');
+//         break;
+//     case "E8":
+//         console.log('White');
+//         break; 
+
+//     // F - Section  
+
+//     case "F1":
+//         console.log('White');
+//         break;       
+//     case "F2":
+//         console.log('Black');
+//         break;
+//     case "F3":
+//         console.log('White');
+//         break;
+//     case "F4":
+//         console.log('Black');
+//         break;
+//     case "F5":
+//         console.log('White'); 
+//         break;      
+//     case "F6":
+//         console.log('Black');
+//         break;
+//     case "F7":
+//         console.log('White');
+//         break;   
+//     case "F8":
+//         console.log('Black');
+//         break;
+        
+//     // G - Section 
+    
+//     case "G1":
+//         console.log('Black');
+//         break;
+//     case "G2":
+//         console.log('White');
+//         break;       
+//     case "G3":
+//         console.log('Black');
+//         break;
+//     case "G4":
+//         console.log('White');
+//         break;
+//     case "G5":
+//         console.log('Black');
+//         break;
+//     case "G6":
+//         console.log('White'); 
+//         break;      
+//     case "G7":
+//         console.log('Black');
+//         break;
+//     case "G8":
+//         console.log('White');
+//         break; 
+
+//     // H - Section  
+    
+//     case "H1":
+//         console.log('White');
+//         break;       
+//     case "H2":
+//         console.log('Black');
+//         break;
+//     case "H3":
+//         console.log('White');
+//         break;
+//     case "H4":
+//         console.log('Black');
+//         break;
+//     case "H5":
+//         console.log('White'); 
+//         break;      
+//     case "H6":
+//         console.log('Black');
+//         break;
+//     case "H7":
+//         console.log('White');
+//         break;   
+//     case "H8":
+//         console.log('Black');
+//         break;                
+// }
+
+// Task - 3
+// for(let i = 1; i <= 90; i++){
+//     if(i % 5 === 0){
+//         console.log(i)
+//     }
+// }
+
+// Task - 5
+
+// let names = ['john', 'defoe', 'karl','enrique','miguel','roberto'];
+// let myFriends = [names[[4]] += ' ' + names[[2]] + ' ' + names[[5]]]
+// console.log(myFriends)
+
+// Task - 6
+
+// let i = 7;
+// while(true){
+//    i--
+//    if(i === 0){
+//     break;
+//    }
+//    console.log(i)
+// }
+
+// Task - 7
+
+// let str = 'Javascript is better';
+// for(let i = 0; i < str.split('').length; i++){
+//     console.log(str[i])
+// }
+
+// Task - 8
+// let a = prompt("UserName")
+// if(a === 'gandi123'){
+//     alert(`'Welcome'${a}`)
+// }
+// else{
+//     alert('Try Again')
+// }
+
+// Task - 9
+
+// for(let i = 0; i <= 40; i++){
+//     if(i % 2 == 0){
+//         console.log(i)
+//     }
+// }
+
+// Task - 10
+
+// let arr = []
+// for(let i = 0; i < 10; i++){
+//     let random = Math.floor(Math.random() * 41)
+//     arr.push(random)
+// }
+// console.log(arr)
+
+// Task - 11
+// let arr = []
+// for(let i = 1; i < 10; i++){
+//     arr.push(i ** 2)
+// }
+// console.log(arr)
+
+// 4 , 15 , 14 , 12, 13 - bajarilmadi
